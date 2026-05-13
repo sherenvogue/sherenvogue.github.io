2f54af8aa7ba7e2eb1e72f9868e82ad992fd86a2
Md. Rafiul Islam <rafiul.i1@uttarauniversity.edu.bd>
2026-05-13 15:04:58 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// SherenVogue JavaScript
+
+// Cart Icon click alert
+const cartIcon = document.querySelector('.cart-icon');
+
+cartIcon.addEventListener('click', function() {
+    alert('Welcome to SherenVogue! Cart and checkout system will be integrated soon.');
+});
